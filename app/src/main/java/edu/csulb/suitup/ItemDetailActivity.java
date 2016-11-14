264dd9f6a0710c25c0f07af90969cdcc126fde6e
thanhnhut90 <thanhnhut91@yahoo.com>
2016-11-13 21:18:43 -0800

    Add grid view on the main wardrobe mgmt page

---
+++ app/src/main/java/edu/csulb/suitup/ItemDetailActivity.java
@@ -0,0 +1,30 @@
+package edu.csulb.suitup;
+
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+/**
+ * Created by Nhut on 11/13/2016.
+ */
+
+public class ItemDetailActivity extends AppCompatActivity{
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.item_detail_layout);
+
+            String title = getIntent().getStringExtra("title");
+            Bitmap bitmap = getIntent().getParcelableExtra("image");
+
+            TextView titleTextView = (TextView) findViewById(R.id.title);
+            titleTextView.setText(title);
+
+            ImageView imageView = (ImageView) findViewById(R.id.image);
+            imageView.setImageBitmap(bitmap);
+        }
+    }
+
--- app/src/main/java/edu/csulb/suitup/WardrobeActivity.java
+++ app/src/main/java/edu/csulb/suitup/WardrobeActivity.java
@@ -1,17 +1,29 @@
 package edu.csulb.suitup;
 
 import android.Manifest;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.content.res.TypedArray;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.GridView;
 import android.widget.Toast;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+
+import static java.lang.System.exit;
 
 /**
  * Created by Nhut on 11/13/2016.
@@ -19,20 +31,14 @@ import java.io.File;
 
 public class WardrobeActivity extends AppCompatActivity {
     private static final int MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE = 1;
-    private String len = "something";
-    Button checkBtn;
+    private GridView gridView;
+    private GridViewAdapter gridAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wrb_mgmt);
-        File dir = new File("/storage/emulated/0/DCIM/Camera");
-        File[] files = dir.listFiles();
-        if (files != null)
-            len = "----" + files.length;
-        else
-            len = "Null";
 
-        checkBtn = (Button)findViewById(R.id.tag_button);
+
         if (ContextCompat.checkSelfPermission(WardrobeActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE)!= PackageManager.PERMISSION_GRANTED)
         {
             //whatever
@@ -45,12 +51,23 @@ public class WardrobeActivity extends AppCompatActivity {
                 ActivityCompat.requestPermissions(WardrobeActivity.this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE);
             }
         }
+        gridView = (GridView) findViewById(R.id.gridView);
+        gridAdapter = new GridViewAdapter(this, R.layout.wrb_item_gridview_layout, getData());
+        gridView.setAdapter(gridAdapter);
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
+                ImageItem item = (ImageItem) parent.getItemAtPosition(position);
+                //Create intent
+                Intent intent = new Intent(WardrobeActivity.this, ItemDetailActivity.class);
+                intent.putExtra("title", item.getTitle());
+                intent.putExtra("image", item.getImage());
+
+                //Start details activity
+                startActivity(intent);
+            }
+        });
     }
-    public void onClick(View view)
-    {
-        if (view.getId() == R.id.tag_button)
-            Toast.makeText(WardrobeActivity.this, "There is some files: " + len, Toast.LENGTH_SHORT).show();
-    }
+
     @Override
     public void onRequestPermissionsResult(int requestCode,
                                            String permissions[], int[] grantResults) {
@@ -60,20 +77,16 @@ public class WardrobeActivity extends AppCompatActivity {
                 if (grantResults.length > 0
                         && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
 
-
-                    File dir = new File(Environment.DIRECTORY_DOWNLOADS);
-                    File[] files = dir.listFiles();
-                    if (files != null)
-                        len = "----" + files.length;
-                    else
-                        len = "Null";
-
                 }
                 else
                 {
-                    Toast.makeText(WardrobeActivity.this,"Boo! Not Granted",Toast.LENGTH_SHORT).show();
                     // permission denied, boo! Disable the
                     // functionality that depends on this permission.
+                    Toast.makeText(WardrobeActivity.this,"We need this permission to+" +
+                            "list your clothes. Please reinstall the app+" +
+                            "and allow us to access the storage",Toast.LENGTH_LONG).show();
+                    Uri packageURI = Uri.parse("package:edu.csulb.suitup");
+                    Intent uninstallIntent = new Intent(Intent.ACTION_DELETE, packageURI);
                 }
                 return;
             }
@@ -82,4 +95,50 @@ public class WardrobeActivity extends AppCompatActivity {
             // permissions this app might request
         }
     }
+    public File[] getFiles()
+    {
+        String directory = "/storage/emulated/0/DCIM/Test";
+        File dir = new File(directory);
+        File[] files = dir.listFiles();
+
+        if (files != null)
+            return files;
+        else
+            throw new NullPointerException("There is no files in the directory");
+    }
+
+    private ArrayList<ImageItem> getData() {
+        File[] nFiles;
+        String imgPath = "/storage/emulated/0/DCIM/Test";
+        final ArrayList<ImageItem> imageItems = new ArrayList<>();
+
+        try
+        {
+            nFiles = getFiles();
+        }
+        catch (NullPointerException e)
+        {
+            System.err.println("NullPointerException: " + e.getMessage());
+        }
+        finally {
+            nFiles = new File(imgPath).listFiles();
+        }
+
+
+        for (int i = 0; i < nFiles.length; i++)
+        {
+            try {
+                File f=new File(imgPath, nFiles[i].getName());
+                Bitmap b = BitmapFactory.decodeStream(new FileInputStream(f));
+                imageItems.add(new ImageItem(b, "Image#" + i));
+            }
+            catch (FileNotFoundException e)
+            {
+                e.printStackTrace();
+            }
+        }
+        return imageItems;
+    }
+
 }
+
